9e7b56291b9f74a9a931ba3044a39b2c0b6afdce
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-01 08:24:22 +0530

    Add Entity class Student with require annotations.

---
+++ src/main/java/com/reactive/practice/Students.java
@@ -0,0 +1,15 @@
+package com.reactive.practice;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table("students")
+public class Students {
+}
