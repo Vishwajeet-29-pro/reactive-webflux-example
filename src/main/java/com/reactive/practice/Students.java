98d81eb783c4dd5c9193802872823d6d0dc6a790
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-01 08:31:11 +0530

    Add fields to entity class.

--- src/main/java/com/reactive/practice/Students.java
+++ src/main/java/com/reactive/practice/Students.java
@@ -6,10 +6,26 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.relational.core.mapping.Table;
 
+import java.util.Date;
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Table("students")
 public class Students {
+
+    private UUID uuid;
+    private String first_name;
+    private String last_name;
+    private int age;
+    private String email;
+    private String address;
+    private String gender;
+    private Date enrollment_date;
+    private String course;
+    private double percentage;
+    private String status;
+
 }
