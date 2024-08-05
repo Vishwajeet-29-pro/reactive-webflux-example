b765300c955f92ccf11f81fc7ef75f5c215759f3
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-05 22:36:04 +0530

    feat: Add failing test case for create student.

--- src/test/java/com/reactive/practice/StudentServiceTest.java
+++ src/test/java/com/reactive/practice/StudentServiceTest.java
@@ -1,7 +1,58 @@
 package com.reactive.practice;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
-@SpringBootTest
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 public class StudentServiceTest {
+
+    @Mock
+    private StudentRepository studentRepository;
+
+    @InjectMocks
+    private StudentService studentService;
+
+    private Student student;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        student = new Student();
+        student.setUuid(UUID.randomUUID());
+        student.setFirst_name("John");
+        student.setLast_name("Wick");
+        student.setAge(17);
+        student.setEmail("johnwick22@example.com");
+        student.setGender("Male");
+        student.setAddress("123 Lane Xyz city");
+        student.setEnrollment_date(LocalDateTime.now());
+        student.setCourse("Computer science");
+        student.setStatus("Active");
+        student.setPercentage(89.5);
+    }
+
+    @Test
+    public void createStudent_ShouldReturnsStudent() {
+        when(studentRepository.save(any(Student.class))).thenReturn(Mono.just(student));
+
+        Mono<Student> createdStudent = studentService.createStudent(student);
+
+        StepVerifier.create(createdStudent)
+                .expectNext(student)
+                .verifyComplete();
+
+        verify(studentRepository, times(1)).save(any(Student.class));
+    }
 }
