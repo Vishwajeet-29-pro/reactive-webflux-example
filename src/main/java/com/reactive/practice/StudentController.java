dc743723c963019a089f04d7e6d166a67bfb84c0
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-08 21:39:38 +0530

    feat: Add method for get all students and pass the test case.

--- src/main/java/com/reactive/practice/StudentController.java
+++ src/main/java/com/reactive/practice/StudentController.java
@@ -3,10 +3,8 @@ package com.reactive.practice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -20,5 +18,10 @@ public class StudentController {
     public Mono<ResponseEntity<Student>> createStudent(@RequestBody Student student) {
         return studentService.createStudent(student)
                 .map(createdStudent -> ResponseEntity.status(HttpStatus.CREATED).body(createdStudent));
+    }
+
+    @GetMapping
+    public Flux<Student> getAllStudent() {
+        return studentService.getAllStudents();
     }
 }
