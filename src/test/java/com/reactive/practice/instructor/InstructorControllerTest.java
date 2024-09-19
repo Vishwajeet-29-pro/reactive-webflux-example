3fcf587236c31554cdf10e88b16d02d55661ba37
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-09-19 22:23:22 +0530

    test: Add setup for Instructor controller.

--- src/test/java/com/reactive/practice/instructor/InstructorControllerTest.java
+++ src/test/java/com/reactive/practice/instructor/InstructorControllerTest.java
@@ -1,4 +1,32 @@
 package com.reactive.practice.instructor;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.UUID;
+
+@WebFluxTest(InstructorController.class)
 public class InstructorControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private InstructorService instructorService;
+
+    private Instructors instructors;
+
+    @BeforeEach
+    public void setup() {
+        instructors = new Instructors();
+        instructors.setId(UUID.randomUUID());
+        instructors.setFirstName("Richard");
+        instructors.setLastName("Hendricks");
+        instructors.setEmail("richardh@example.com");
+        instructors.setDepartment("Computer");
+        instructors.setStudentIds(null);
+    }
 }
